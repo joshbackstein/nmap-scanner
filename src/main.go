6cea6dcb30f864e9ef4f1fdcfbbc52f5a0a11b72
joshbackstein <josh.backstein@gmail.com>
2021-07-16 02:54:02 -0600

    Use environment variables

--- src/main.go
+++ src/main.go
@@ -5,19 +5,19 @@ import (
 	_ "github.com/go-sql-driver/mysql"
 	"log"
 	"net/http"
+	"os"
 )
 
 var db *sql.DB
 
 func main() {
-	// TODO: Get these from environment variables with default values
 	// Variables
-	HTTP_PORT := "8080"
-	MYSQL_USERNAME := "root"
-	MYSQL_PASSWORD := "password"
-	MYSQL_HOST := "localhost"
-	MYSQL_PORT := "3306"
-	MYSQL_DB := "NmapScanner"
+	HTTP_PORT := getEnvOrDefault("HTTP_PORT", "8080")
+	MYSQL_USERNAME := getEnvOrDefault("MYSQL_USERNAME", "root")
+	MYSQL_PASSWORD := getEnvOrDefault("MYSQL_PASSWORD", "password")
+	MYSQL_HOST := getEnvOrDefault("MYSQL_HOST", "localhost")
+	MYSQL_PORT := getEnvOrDefault("MYSQL_PORT", "3306")
+	MYSQL_DB := getEnvOrDefault("MYSQL_DB", "NmapScanner")
 
 	// DB setup
 	connectionString := MYSQL_USERNAME + ":" + MYSQL_PASSWORD + "@tcp(" + MYSQL_HOST + ":" + MYSQL_PORT + ")/" + MYSQL_DB
@@ -33,4 +33,12 @@ func main() {
 	router := NewRouter()
 	log.Println("Listening for HTTP requests on port " + HTTP_PORT)
 	log.Fatal(http.ListenAndServe(":"+HTTP_PORT, router))
+}
+
+func getEnvOrDefault(envVariable string, def string) string {
+	var value = os.Getenv("HTTP_PORT")
+	if len(value) > 0 {
+		return value
+	}
+	return def
 }
